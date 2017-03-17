ce798e846a9542778cc399be4bfda9e68e4b2804
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-03-17 14:10:17 +0200

    Initial commit

---
+++ src/DocumentInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Document
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types = 1);
+
+namespace Vainyl\Document;
+
+use Vainyl\Core\ArrayX\ArrayInterface;
+
+/**
+ * Interface DocumentInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+interface DocumentInterface extends ArrayInterface
+{
+}
---
+++ src/Repository/DocumentRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Document
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types = 1);
+
+namespace Vainyl\Document\Repository;
+
+use Vainyl\Core\Name\NameableInterface;
+use Vainyl\Document\DocumentInterface;
+
+/**
+ * Interface DocumentRepositoryInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+interface DocumentRepositoryInterface extends NameableInterface
+{
+    /**
+     * @param string $id
+     *
+     * @return null|DocumentInterface
+     */
+    public function getDocument(string $id) : ?DocumentInterface;
+}
