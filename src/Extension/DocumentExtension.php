19deca862d59871d519b123b26467f02baec1822
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-05-10 15:04:07 +0300

    Added DocumentExtension

---
+++ src/Extension/DocumentExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Document
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Document\Extension;
+
+use Vainyl\Core\Extension\AbstractExtension;
+
+/**
+ * Class DocumentExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DocumentExtension extends AbstractExtension
+{
+}
