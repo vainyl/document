1a615f08f38aee2b8fbebdad3b036d3b030db26c
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-14 14:34:42 +0300

    Added DocumentConfiguration

--- src/Extension/DocumentExtension.php
+++ src/Extension/DocumentExtension.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace Vainyl\Document\Extension;
 
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Vainyl\Core\Extension\AbstractExtension;
 
 /**
@@ -21,4 +23,25 @@ use Vainyl\Core\Extension\AbstractExtension;
  */
 class DocumentExtension extends AbstractExtension
 {
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     *
+     * @return AbstractExtension
+     */
+    public function load(array $configs, ContainerBuilder $container): AbstractExtension
+    {
+        $configuration = new DocumentConfiguration();
+        $documentConfiguration = $this->processConfiguration($configuration, $configs);
+
+        $container->removeDefinition('database.entity');
+        $container->removeDefinition('document.operation.factory');
+        $container->setAlias('database.entity', new Alias('database.entity.' . $documentConfiguration['odm']));
+        $container->setAlias(
+            'document.operation.factory',
+            new Alias('document.operation.factory' . $documentConfiguration['odm'])
+        );
+
+        return parent::load($configs, $container);
+    }
 }
